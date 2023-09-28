a4aab9e67465d61e73668b840c7dc981be333f1d
wuxuwei <wuxuwei@codemao.cn>
2023-09-28 15:27:28 +0800

    chore(deps): update dependencies

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ContentModule } from './modules/content/content.module';
+
+@Module({
+    imports: [ContentModule],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
